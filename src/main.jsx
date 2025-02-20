97e785f0df07c042631ec28bd98caab2c53da6a3
Minmgf <manuelinm2015@gmail.com>
2025-02-20 09:51:56 -0500

    Añadir rutas para la navegación entre la aplicación principal y la página de inicio de sesión

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
+import Login from './app/auth/Login.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+    <Routes>
+      <Route path='/' component={<App/>}/>
+      <Route path='/login' component={<Login/>}/>
+    </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
